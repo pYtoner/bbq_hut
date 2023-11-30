653b4d62b6c608a5dbbf8fb1e2398ad91b6b12f9
nitram <villam.martin@gmail.com>
2023-11-30 14:50:13 +0000

    set all for all

--- test_blink.ino
+++ test_blink.ino
@@ -17,65 +17,18 @@ int nLeds[6] = {
 
 void setup() {
   FastLED.addLeds<WS2812B, 6, GRB>(leds, NUM_LEDS);
-  // setAllBlack(0);
-  // delay(20);
-  // setAllBlack(1);
-  // delay(20);
-  // setAllBlack(2);
-  // delay(20);
-
-  
+  setAllBlack();
+  delay(20);
 }
 
 void loop() {
-  setAll(0, CRGB(255, 0, 0));
+  setAll(CRGB(255, 0, 0));
   delay(100);
 
-  rainbowUpPattern();
+  rainbowPattern(true);
 }
 
-void rainbowUpPattern() {
-  CRGB colors[85];
-  CRGB shifted[85];
-
-  for (int i = 0; i <= 85 - 1; i++) {
-    colors[i] = CRGB(255, 0, 0);
-  }
-
-  for (int i = 0; i <= 85 - 1; i++) {
-    shifted[i] = CRGB(255, 0, 0);
-  }
-
-  while (true) {
-    for (int i = 0; i <= 85 - 1; i++) {
-      shifted[i] = colors[(i + 84) % 85];
-
-      // change color
-      CHSV newColor = rgb2hsv_approximate(shifted[i]);
-      int hue = newColor.hue;
-      hue += 20;
-      hue %= 255;
-      shifted[83].setHue(hue);
-    }
-  
-    for (int i = 0; i <= 85 - 1; i++) {
-      colors[i] = shifted[i];
-    }
-
-    for (int i = 0; i <= 85 - 1; i++) {
-      leds[middleIndex(0, i)] = colors[i];
-      leds[middleIndex(0, 170-i)] = colors[i];
-      leds[middleIndex(1, i)] = colors[i];
-      leds[middleIndex(1, 170-i)] = colors[i];
-      leds[middleIndex(2, i)] = colors[i];
-      leds[middleIndex(2, 170-i)] = colors[i];
-    }
-    FastLED.show();
-    delay(40);
-  }
-}
-
-void rainbowDownPattern() {
+void rainbowPattern(bool isUp) {
   CRGB colors[85];
   CRGB shifted[85];
 
@@ -104,27 +57,48 @@ void rainbowDownPattern() {
     }
 
     for (int i = 0; i <= 85 - 1; i++) {
-      leds[middleIndex(0, i)] = colors[i];
-      leds[middleIndex(0, 170-i)] = colors[i];
-      leds[middleIndex(1, i)] = colors[i];
-      leds[middleIndex(1, 170-i)] = colors[i];
-      leds[middleIndex(2, i)] = colors[i];
-      leds[middleIndex(2, 170-i)] = colors[i];
+      if isUp {
+        leds[middleTopIndex(0, i)] = colors[i];
+        leds[middleTopIndex(0, 170-i)] = colors[i];
+        leds[middleTopIndex(1, i)] = colors[i];
+        leds[middleTopIndex(1, 170-i)] = colors[i];
+        leds[middleTopIndex(2, i)] = colors[i];
+        leds[middleTopIndex(2, 170-i)] = colors[i];
+      } else {
+        leds[middleIndex(0, i)] = colors[i];
+        leds[middleIndex(0, 170-i)] = colors[i];
+        leds[middleIndex(1, i)] = colors[i];
+        leds[middleIndex(1, 170-i)] = colors[i];
+        leds[middleIndex(2, i)] = colors[i];
+        leds[middleIndex(2, 170-i)] = colors[i];
+      }
     }
     FastLED.show();
     delay(40);
   }
 }
 
-void setAllBlack(int tapestryIdx) {
+void setTapestryBlack(int tapestryIdx) {
   setAll(tapestryIdx, CRGB(0, 0, 0));
 }
 
-void setAll(int tapestryIdx, CRGB color) {
+void setTapestry(int tapestryIdx, CRGB color) {
   for (int i = 0; i <= 170 - 1; i++) {
     leds[index(tapestryIdx, i)] = color;
   }
   FastLED.show();
+}
+
+void setAllBlack() {
+  for (int i = 0; i <= 6 - 1; i++) {
+    setTapestryBlack(i);
+  }
+}
+
+void setAll(CRGB color) {
+  for (int i = 0; i <= 6 - 1; i++) {
+    setTapestry(i, color);
+  }
 }
 
 int offsetForBottomMiddle[6] = {
